aae4a7a1147b727d82f5f3218610652065f6d1a2
Said-Khasanov <said.hasanow@yandex.ru>
2024-04-24 20:57:03 +0400

    create new branch, add liquibase

---
+++ src/main/java/com/javarush/khasanov/util/LiquibaseInit.java
@@ -0,0 +1,24 @@
+package com.javarush.khasanov.util;
+
+import liquibase.Scope;
+import liquibase.command.CommandScope;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+public class LiquibaseInit {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Running Liquibase...");
+
+        Scope.child(Scope.Attr.resourceAccessor, new ClassLoaderResourceAccessor(), () -> {
+            CommandScope update = new CommandScope("update");
+
+            update.addArgumentValue("changelogFile", "db/changelog.xml");
+            update.addArgumentValue("url", "jdbc:postgresql://localhost:5432/dev");
+            update.addArgumentValue("username", "admin");
+            update.addArgumentValue("password", "admin");
+
+            update.execute();
+        });
+
+        System.out.println("Running Liquibase...DONE");
+    }
+}
